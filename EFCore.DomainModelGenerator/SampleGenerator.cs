62f0665bd76c68d8bfb2017e8b20c0323f4c063e
Tsubasa <26428614+kamome283@users.noreply.github.com>
2025-11-23 18:15:33 +0900

    add: sample generator which does nothing

---
+++ EFCore.DomainModelGenerator/SampleGenerator.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace EFCore.DomainModelGenerator;
+
+[Generator(LanguageNames.CSharp)]
+public partial class SampleGenerator : IIncrementalGenerator
+{
+  public void Initialize(IncrementalGeneratorInitializationContext context)
+  {
+  }
+}
