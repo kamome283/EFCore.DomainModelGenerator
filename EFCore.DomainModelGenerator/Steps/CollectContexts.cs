5b50c7ac6719b06b4d66c6195928c90846474b7d
Tsubasa <26428614+kamome283@users.noreply.github.com>
2025-12-04 20:09:13 +0900

    Remove domain namespace collection from the context gathering step.

--- EFCore.DomainModelGenerator/Steps/CollectContexts.cs
+++ EFCore.DomainModelGenerator/Steps/CollectContexts.cs
@@ -2,8 +2,6 @@ using Microsoft.CodeAnalysis;
 
 namespace EFCore.DomainModelGenerator.Steps;
 
-using static Common;
-
 internal static class CollectContexts
 {
   public const string TargetAttribute = "DomainContextAttribute";
@@ -11,11 +9,8 @@ internal static class CollectContexts
   public static ContextMetadata Collect(GeneratorAttributeSyntaxContext source, CancellationToken _)
   {
     var symbol = source.TargetSymbol as INamedTypeSymbol ?? throw new CollectContextsException("symbol");
-    var attr = symbol.GetAttributesOf($"{GeneratorNamespace}.{TargetAttribute}").SingleOrDefault()
-               ?? throw new CollectContextsException("attr");
     return new ContextMetadata
     {
-      Namespace = attr.GetArgumentAt(0) as string ?? throw new CollectContextsException("Namespace"),
       ContextType = symbol,
     };
   }
@@ -23,7 +18,6 @@ internal static class CollectContexts
 
 internal record ContextMetadata
 {
-  public string Namespace { get; set; } = null!;
   public INamedTypeSymbol ContextType { get; set; } = null!;
 }
 
