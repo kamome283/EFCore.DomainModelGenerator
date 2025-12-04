fc551f24318f81024547d5a7bcbc130d07b33fe4
Tsubasa <26428614+kamome283@users.noreply.github.com>
2025-12-04 15:02:26 +0900

    Refactor the complex metadata combination step

--- EFCore.DomainModelGenerator/Steps/CombineMetadata.cs
+++ EFCore.DomainModelGenerator/Steps/CombineMetadata.cs
@@ -8,8 +8,7 @@ internal static class CombineMetadata
     IEnumerable<SetMetadata> sets,
     CancellationToken _)
   {
-    var impl = new Impl(contexts, models, sets);
-    return impl.Process();
+    return CombineImpl.Process(contexts, models, sets);
   }
 }
 
@@ -20,65 +19,59 @@ internal record MetadataGroup
   public IEnumerable<SetMetadata> Sets { get; set; } = [];
 }
 
-file class Impl(
-  IEnumerable<ContextMetadata> contexts,
-  IEnumerable<ModelMetadata> markedModels,
-  IEnumerable<SetMetadata> sets)
+file static class CombineImpl
 {
-  private readonly Dictionary<string, ContextMetadata> _contexts =
-    contexts.ToDictionary(x => x.ContextType.Name);
-
-  private readonly Dictionary<string, Group> _pairs =
-    markedModels
-      .Select(model => new Group { Model = model })
-      .ToDictionary(pair => pair.ModelName);
-
-  public IEnumerable<MetadataGroup> Process()
+  public static IEnumerable<MetadataGroup> Process(
+    IEnumerable<ContextMetadata> contexts,
+    IEnumerable<ModelMetadata> markedModels,
+    IEnumerable<SetMetadata> sets)
   {
+    var contextMap = contexts
+      .ToDictionary(x => x.ContextType.Name);
+    var groupMap = markedModels
+      .Select(x => new Group { Model = x })
+      .ToDictionary(x => x.ModelName);
+
     foreach (var set in sets)
     {
-      var pair = GetCorrespondingPair(set);
-      pair.Sets.Add(set);
-      var context = GetCorrespondingContext(set);
-      pair.AddContextIfNotExist(context);
+      _ = contextMap.TryGetValue(set.ParentType.Name, out var correspondingContext);
+      if (correspondingContext is null) throw new CombineMetadataException("correspondingContext");
+
+      _ = groupMap.TryGetValue(set.ModelName, out var correspondingGroup);
+      if (correspondingGroup is null)
+      {
+        correspondingGroup = new Group { Model = new ModelMetadata { ModelName = set.ModelName } };
+        groupMap[set.ModelName] = correspondingGroup;
+      }
+
+      correspondingGroup.AddSet(set);
+      correspondingGroup.AddContextIfNotExist(correspondingContext);
     }
 
-    return _pairs.Values.Select(x => new MetadataGroup
+    return groupMap.Values.Select(x => x.ToContextMetadata());
+  }
+
+  private record Group
+  {
+    private List<ContextMetadata> Contexts { get; } = [];
+    public ModelMetadata Model { private get; set; } = null!;
+    private List<SetMetadata> Sets { get; } = [];
+    public string ModelName => Model.ModelName;
+
+    public void AddSet(SetMetadata set) => Sets.Add(set);
+
+    public void AddContextIfNotExist(ContextMetadata context)
     {
-      Contexts = x.Contexts,
-      Model = x.Model,
-      Sets = x.Sets,
-    });
-  }
+      if (Contexts.Any(x => x.ContextType.Name == context.ContextType.Name)) return;
+      Contexts.Add(context);
+    }
 
-  private ContextMetadata GetCorrespondingContext(SetMetadata set)
-  {
-    return _contexts.SingleOrDefault(x => x.Key == set.ParentType.Name).Value ??
-           throw new CombineMetadataException("GetCorrespondingContext");
-  }
-
-  private Group GetCorrespondingPair(SetMetadata set)
-  {
-    var modelName = set.ModelName;
-    if (_pairs.TryGetValue(modelName, out var foundPair)) return foundPair;
-    var model = new ModelMetadata { ModelName = modelName };
-    var pair = new Group { Model = model };
-    _pairs[modelName] = pair;
-    return pair;
-  }
-}
-
-file record Group
-{
-  public List<ContextMetadata> Contexts { get; } = [];
-  public ModelMetadata Model { get; set; } = null!;
-  public List<SetMetadata> Sets { get; } = [];
-  public string ModelName => Model.ModelName;
-
-  public void AddContextIfNotExist(ContextMetadata context)
-  {
-    if (Contexts.Any(x => x.ContextType.Name == context.ContextType.Name)) return;
-    Contexts.Add(context);
+    public MetadataGroup ToContextMetadata() => new()
+    {
+      Contexts = Contexts,
+      Model = Model,
+      Sets = Sets,
+    };
   }
 }
 
